1fd130e1c344bd29e6171fb4290650e89addb3f3
Eduardo Gonzalez <39170372+eduardomarquesgonzalez@users.noreply.github.com>
2020-12-07 17:48:40 -0400

    version 002

--- src/main/java/br/gov/mt/cuiaba/cobranca/controller/TituloController.java
+++ src/main/java/br/gov/mt/cuiaba/cobranca/controller/TituloController.java
@@ -2,9 +2,12 @@ package br.gov.mt.cuiaba.cobranca.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+import br.gov.mt.cuiaba.cobranca.model.StatusTitulo;
 import br.gov.mt.cuiaba.cobranca.model.Titulo;
 import br.gov.mt.cuiaba.cobranca.repository.Titulos;
 
@@ -16,14 +19,20 @@ public class TituloController {
 	private Titulos titulos;
 	
 	@RequestMapping("/novo")
-	public String novo () {
-		return "CadastroTitulo";
+	public ModelAndView novo() {
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject("todosStatusTitulo", StatusTitulo.values());
+		return mv;
 	}
 	
 	@RequestMapping(method = RequestMethod.POST)
-	public String salvar(Titulo titulo) {
-	
+	public ModelAndView salvar(Titulo titulo) {
 		titulos.save(titulo);
-		return "CadastroTitulo";
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		mv.addObject("mensagem","Titulo salvo com sucesso");
+		return mv;
 	}
+	
+	@ModelAttribute
+	public List<s>
 }
--- src/main/java/br/gov/mt/cuiaba/cobranca/repository/Titulos.java
+++ src/main/java/br/gov/mt/cuiaba/cobranca/repository/Titulos.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.gov.mt.cuiaba.cobranca.model.Titulo;
 
 public interface Titulos extends JpaRepository<Titulo, Long>{
-
+	
 }
